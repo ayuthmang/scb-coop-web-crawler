c4ee1ef80914a7495efbc794a9f75b98bd75adbd
Ayuth Mangmesap <ayuth.mang@gmail.com>
2022-07-31 13:55:11 +0700

    refactor: separate each action to a function :ninja:

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
 import dotenv from 'dotenv'
-import puppeteer from 'puppeteer'
+import puppeteer, { Puppeteer } from 'puppeteer'
 
 main()
 
 async function main() {
   await bootstrap()
-  await start({
-    username: process.env.USERNAME!,
-    password: process.env.PASSWORD!,
-  })
+  await start()
 }
 
 async function bootstrap() {
@@ -22,19 +19,34 @@ const URLS = {
   shareStock: `${BASE_URL}/Share/ShareStock.aspx`,
 }
 
-async function start({
-  username,
-  password,
-}: {
-  username: string
-  password: string
-}) {
+async function start() {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: { width: 1280, height: 720 },
   })
   const page = await browser.newPage()
 
+  await login(page, {
+    username: process.env.USERNAME!,
+    password: process.env.PASSWORD!,
+  })
+
+  await gotoShareStockPage(page)
+  await page.screenshot({ path: 'share-stocks.png' })
+
+  await browser.close()
+}
+
+async function login(
+  page: puppeteer.Page,
+  {
+    username,
+    password,
+  }: {
+    username: string
+    password: string
+  }
+) {
   const inputUsernameElmId = '#bodyContent_txtLogin'
   const inputPasswordElmId = '#bodyContent_txtPWD'
   const btnLoginElmId = '#bodyContent_btnSubmit'
@@ -45,20 +57,18 @@ async function start({
   await page.type(inputPasswordElmId, password)
   await page.click(btnLoginElmId)
   await page.waitForNavigation()
+}
 
-  // share stock page
+async function gotoShareStockPage(page: puppeteer.Page) {
   const labelShareStockElmId = '#ContentPlaceHolder1_bodyContent_lblShareStock'
   await page.goto(URLS.shareStock, { waitUntil: 'domcontentloaded' })
   await page.waitForSelector(labelShareStockElmId)
   /*
   const textContent = await page.evaluate(() => {
     // need to pass explicitly to avoid reference error
-    const labelShareStockElmId =
+    const labelShareStockElmIgd =
       '#ContentPlaceHolder1_bodyContent_lblShareStock'
     return document.querySelector(labelShareStockElmId)?.textContent
   })
   */
-  await page.screenshot({ path: 'share-stocks.png' })
-
-  await browser.close()
 }
